1ac6497db5dbdf11347db261e3a90d0aa06e5ed7
Tom Ekander <ekander.tom@gmail.com>
2025-09-24 14:14:15 +0200

    add reason + reason interface

---
+++ languages/reason-interface/outline.scm
@@ -0,0 +1,32 @@
+; module type T = ...
+(module_type_definition
+  "module" @context
+  "type"   @context
+  name: (_) @name) @item
+
+; module M = ...
+(module_definition
+  "module" @context
+  (module_binding
+    name: (module_name) @name)) @item
+
+; type t = ...
+(type_definition
+  "type" @context
+  (type_binding
+    name: (_) @name)) @item
+
+; let foo: t => u;
+(value_specification
+  (value_name) @name) @item
+
+; external foo: ...
+(external
+  "external" @context
+  (value_name) @name) @item
+
+; exception Foo
+(exception_definition
+  "exception" @context
+  (constructor_declaration
+    (constructor_name) @name)) @item
---
+++ languages/reason/indents.scm
@@ -0,0 +1,11 @@
+[
+  (let_binding)
+  (type_binding)
+  (external)
+  (value_specification)
+] @indent
+
+(_ "[" "]" @end) @indent
+(_ "[|" "|]" @end) @indent
+(_ "{" "}" @end) @indent
+(_ "(" ")" @end) @indent
