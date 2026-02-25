732a90f977751a3526f221f56a796f6a5040b306
João Soares <37777652+Dnreikronos@users.noreply.github.com>
2026-02-25 12:55:05 -0300

    Support MLX files (#29)
    Register  files as OCaml to provide syntax highlighting and LSP support
    for the MLX dialect (https://github.com/ocaml-mlx/mlx).
    
    
    Fix issue: #26

---
+++ languages/mlx/indents.scm
@@ -0,0 +1,54 @@
+[
+  (let_binding)
+  (type_binding)
+
+  (method_definition)
+
+  (external)
+  (value_specification)
+  (method_specification)
+
+  (match_case)
+
+  (function_expression)
+
+  (field_declaration)
+  (field_expression)
+
+  (application_expression)
+] @indent
+
+(_ "[" "]" @end) @indent
+(_ "[|" "|]" @end) @indent
+(_ "<" ">" @end) @indent
+(_ "{" "}" @end) @indent
+(_ "(" ")" @end) @indent
+
+(_ "object" @start "end" @end) @indent
+
+(structure
+  "struct" @start
+  "end" @end) @indent
+
+(signature
+  "sig" @start
+  "end" @end) @indent
+
+(parenthesized_expression
+  "begin" @start
+  "end") @indent
+
+(do_clause
+  "do" @start
+  "done" @end) @indent
+
+";;" @outdent
+
+; JSX
+;-----
+
+(jsx_element_opening) @indent
+(jsx_element_self_closing) @indent
+
+(jsx_element_closing ">" @end)
+(jsx_element_self_closing "/>" @end)
