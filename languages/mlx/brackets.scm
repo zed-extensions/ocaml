69290de8889c24122d3db69662e518a27815f914
Emmanuel Arrighi <emmanuel.arrighi@ahrefs.com>
2026-03-06 12:02:41 +0100

    Add let expression bracket pairs to OCaml and MLX

--- languages/mlx/brackets.scm
+++ languages/mlx/brackets.scm
@@ -24,3 +24,19 @@
 (jsx_element_self_closing
   "<" @open
   "/>" @close)
+
+(let_expression
+  (value_definition "let" @open)
+  "in" @close)
+
+(let_module_expression
+  "let" @open
+  "in" @close)
+
+(let_open_expression
+  "let" @open
+  "in" @close)
+
+(let_exception_expression
+  "let" @open
+  "in" @close)
--- languages/ocaml/brackets.scm
+++ languages/ocaml/brackets.scm
@@ -12,3 +12,19 @@
 
 ("\"" @open
   "\"" @close)
+
+(let_expression
+  (value_definition "let" @open)
+  "in" @close)
+
+(let_module_expression
+  "let" @open
+  "in" @close)
+
+(let_open_expression
+  "let" @open
+  "in" @close)
+
+(let_exception_expression
+  "let" @open
+  "in" @close)
